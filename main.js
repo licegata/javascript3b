e647274213774a6ba563538fbf15b6c91c631c01
Alice <102537891+licegata@users.noreply.github.com>
2024-04-29 08:26:22 -0300

    comit

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador"); 
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
@@ -51,8 +51,8 @@ return [0,0,0,0];
 
 function atualizaCronometro(){
     function atualizaCronometro(){
-        document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-        document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+        document.getElementById("dias").textContent = calculaTempo(tempos[0])[0];
+        document.getElementById("horas").textContent = calculaTempo(tempos[0])[1];
         document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
         document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
     
